7728fd66d6b0afd5b2dc4c531792999ec8db9efd
Tomas Ravinskas <tomasrav@gmail.com>
2017-05-13 01:55:13 +0300

    - Fixed token refresh error.

--- src/Sms.py
+++ src/Sms.py
@@ -161,6 +161,8 @@ class GoogleContacts(object):
 				space_requests, connections = get_connections()
 			except TokenExpiredError:
 				self.refresh_token()
+				google = OAuth2Session(
+					client_id=self.client_id, token=self.token)
 				space_requests, connections = get_connections()
 		else:
 			self.get_consent()
